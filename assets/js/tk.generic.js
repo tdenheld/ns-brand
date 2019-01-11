b2e3d4d9050905bbff691feb1775218f42eaada9
tdenheld <tibor@tk305.com>
2019-01-11 14:38:21 +0100

    #fix RP: update ns.generic.js

--- assets/js/tk.generic.js
+++ assets/js/tk.generic.js
@@ -1,12 +1,12 @@
 // generic javascript
 // ------------------------------------------------------------
-$(document).ready(function(){
+
 
 // global vars
 // ------------------------------------------------------------
 // ------------------------------------------------------------
-var window_width = Number;
-var window_height = Number;
+var vw = Number;
+var vh = Number;
 var mobile = Boolean;
 var scrolled = false;
 
@@ -15,11 +15,11 @@ var scrolled = false;
 // update screen height
 // ------------------------------------------------------------	
 function update_window_size() {
-	window_width = $(window).innerWidth();
-	window_height = $(window).innerHeight();
+	vw = $(window).innerWidth();
+	vh = $(window).innerHeight();
 	
 	// set breakpoints
-	if (window_width > 912) {
+	if (vw > 912) {
 		mobile = false;
 	} else {
 		mobile = true;
@@ -50,44 +50,3 @@ function scrolling() {
 	// fade arrow scroll down button
 	scrolled = false;
 };
-
-
-
-
-
-// include html
-// ------------------------------------------------------------
-function include_html() {
-    var z, i, elmnt, file, xhttp;
-    /*loop through a collection of all HTML elements:*/
-    z = document.getElementsByTagName("*");
-    for (i = 0; i < z.length; i++) {
-        elmnt = z[i];
-        /*search for elements with a certain atrribute:*/
-        file = elmnt.getAttribute("include");
-        if (file) {
-        	/*make an HTTP request using the attribute value as the file name:*/
-            xhttp = new XMLHttpRequest();
-            xhttp.onreadystatechange = function () {
-                if (this.readyState == 4) {
-                    if (this.status == 200) {
-                        elmnt.innerHTML = this.responseText;
-                    }
-                    if (this.status == 404) {
-                        elmnt.innerHTML = "Page not found.";
-                    }
-                    /*remove the attribute, and call this function once more:*/
-                    elmnt.removeAttribute("include");
-                    include_html();
-                };
-            };
-            xhttp.open("GET", file, true);
-            xhttp.send();
-            /*exit the function:*/
-            return;
-        };
-    };
-};
-include_html();
-
-});
